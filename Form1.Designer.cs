ae0cfcb7ce63d18314d48e93a20b637dac630e8d
bartrze52 <114146707+bartrze52@users.noreply.github.com>
2022-11-05 15:33:20 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace aaaaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.aktor = new System.Windows.Forms.TextBox();
+            this.tytul = new System.Windows.Forms.TextBox();
+            this.rezyser = new System.Windows.Forms.TextBox();
+            this.data = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.HedTytul = new System.Windows.Forms.ColumnHeader();
+            this.colHedReżyser = new System.Windows.Forms.ColumnHeader();
+            this.colHedData = new System.Windows.Forms.ColumnHeader();
+            this.colHedAktor = new System.Windows.Forms.ColumnHeader();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.usuńWybraneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tytuł:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reżyser:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(633, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data premiery:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Główny aktor(ka):";
+            // 
+            // aktor
+            // 
+            this.aktor.Location = new System.Drawing.Point(818, 165);
+            this.aktor.Name = "aktor";
+            this.aktor.Size = new System.Drawing.Size(125, 27);
+            this.aktor.TabIndex = 4;
+            this.aktor.Tag = "aktor";
+            // 
+            // tytul
+            // 
+            this.tytul.Location = new System.Drawing.Point(215, 56);
+            this.tytul.Name = "tytul";
+            this.tytul.Size = new System.Drawing.Size(125, 27);
+            this.tytul.TabIndex = 5;
+            this.tytul.Tag = "tytul";
+            // 
+            // rezyser
+            // 
+            this.rezyser.Location = new System.Drawing.Point(215, 161);
+            this.rezyser.Name = "rezyser";
+            this.rezyser.Size = new System.Drawing.Size(125, 27);
+            this.rezyser.TabIndex = 6;
+            this.rezyser.Tag = "rezyser";
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(818, 59);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(250, 27);
+            this.data.TabIndex = 7;
+            this.data.Tag = "data";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(818, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(818, 629);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Zamknij";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(663, 629);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Zapisz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.HedTytul,
+            this.colHedReżyser,
+            this.colHedData,
+            this.colHedAktor});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(42, 334);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1052, 271);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // HedTytul
+            // 
+            this.HedTytul.Text = "Tytuł";
+            this.HedTytul.Width = 263;
+            // 
+            // colHedReżyser
+            // 
+            this.colHedReżyser.Text = "Reżyser";
+            this.colHedReżyser.Width = 263;
+            // 
+            // colHedData
+            // 
+            this.colHedData.Text = "Data";
+            this.colHedData.Width = 263;
+            // 
+            // colHedAktor
+            // 
+            this.colHedAktor.Text = "Aktor";
+            this.colHedAktor.Width = 263;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuńWybraneToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(171, 28);
+            // 
+            // usuńWybraneToolStripMenuItem
+            // 
+            this.usuńWybraneToolStripMenuItem.Name = "usuńWybraneToolStripMenuItem";
+            this.usuńWybraneToolStripMenuItem.Size = new System.Drawing.Size(170, 24);
+            this.usuńWybraneToolStripMenuItem.Text = "Usuń wybrane";
+            this.usuńWybraneToolStripMenuItem.Click += new System.EventHandler(this.usuńWybraneToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1241, 682);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.rezyser);
+            this.Controls.Add(this.tytul);
+            this.Controls.Add(this.aktor);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox aktor;
+        private System.Windows.Forms.TextBox tytul;
+        private System.Windows.Forms.TextBox rezyser;
+        private System.Windows.Forms.DateTimePicker data;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader HedTytul;
+        private System.Windows.Forms.ColumnHeader colHedReżyser;
+        private System.Windows.Forms.ColumnHeader colHedData;
+        private System.Windows.Forms.ColumnHeader colHedAktor;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem usuńWybraneToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aaaaa
+{
+
+    public partial class Form1 : Form
+    {
+        private void DodawanieDanych(string tytul, string rezyser, string data, string aktor)
+        {
+            ListViewItem item = new ListViewItem(new string[] { tytul, rezyser, data, aktor });
+            listView1.Items.Add(item);
+        }
+
+        private void DodawanieDanych(string[] dane)
+        {
+            ListViewItem item = new ListViewItem(dane);
+            listView1.Items.Add(item);
+        }
+
+        private void UsuwanieDanych()
+        {
+            foreach(ListViewItem item in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(item);
+            }
+        }
+
+        private string[] WierszeDoPliku()
+        {
+            string[] linie = new string[listView1.Items.Count];
+            int i = 0;
+            foreach(ListViewItem item in listView1.Items)
+            {
+                linie[i] = "";
+                for (int k = 0; k < item.SubItems.Count; k++)
+                    linie[i] += item.SubItems[k].Text + "*";
+                i++;
+            }
+            return linie;
+        }
+
+        private void BtnSave_Click(object Sender, EventArgs e)
+        {
+            string[] linie = WierszeDoPliku();
+            File.WriteAllLines("filmy.txt", linie);
+        }
+        private void OdczytZPliku()
+        {
+            if (!File.Exists("filmy.txt"))
+                return;
+
+            string[] linie = File.ReadAllLines("filmy.txt");
+            foreach(string linia in linie)
+            {
+                string[] temp = linia.Split('*');
+                DodawanieDanych(temp);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usuńWybraneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UsuwanieDanych();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DodawanieDanych(tytul.Text, rezyser.Text, data.Text, aktor.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OdczytZPliku();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] linie = WierszeDoPliku();
+
+            File.WriteAllLines("filmy.txt", linie);
+        }
+    }
+}
